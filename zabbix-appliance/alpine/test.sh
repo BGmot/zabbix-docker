49ec20d0e6d3a238043cf02bb2d3d05dd3770670
Evgeny Yurchenko <evgeny.yurchenko@bmo.com>
2019-11-12 19:04:28 -0500

    An attempt #5 to fix Docker Hub tests.

--- zabbix-appliance/alpine/test.sh
+++ zabbix-appliance/alpine/test.sh
@@ -1,1 +1,7 @@
-../../test.sh
+#!/usr/bin/bash
+
+# Wait for container to provision DB and start all the services
+sleep 60
+
+# If these three processes up then most probably deployment went OK
+ps -C mysqld && ps -C zabbix_server && ps -C nginx
