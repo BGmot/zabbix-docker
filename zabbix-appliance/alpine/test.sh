5a1679dc4c4b746126720f48ef3d9878085d024b
Evgeny Yurchenko <evgeny.yurchenko@bmo.com>
2019-11-12 20:20:56 -0500

    An attempt #6 to fix Docker Hub tests.

--- zabbix-appliance/alpine/test.sh
+++ zabbix-appliance/alpine/test.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
 
-# Wait for container to provision DB and start all the services
-sleep 60
+set -eo pipefail
+
+su mysql -c "/usr/bin/mysqld --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mysql/plugin --user=mysql --log-output=none --pid-file=/var/lib/mysql/mysqld.pid --port=3306 --console" &
+
+su zabbix -c "/usr/sbin/zabbix_server --foreground -c /etc/zabbix/zabbix_server.conf" &
+
+su nginx -c "/usr/sbin/nginx "daemon off;" -c /etc/nginx/nginx.conf" &
+
+sleep 5
 
 # If these three processes up then most probably deployment went OK
 ps -C mysqld && ps -C zabbix_server && ps -C nginx
